869a96b3ea8bea0a1f27cf3881abdd813855e057
chrisdoan <doantu@mail.uc.edu>
2021-08-28 18:31:26 -0400

    3rd commit
    Updated the hi script with a comment

--- Assets/Hi.cs
+++ Assets/Hi.cs
@@ -7,7 +7,7 @@ public class Hi : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //A new comment
     }
 
     // Update is called once per frame
